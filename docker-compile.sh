4a807244b9ff4bc4b68857a3887d17c482d688ca
Nathan Agez <nathan.agez@epitech.eu>
2018-04-13 00:15:23 +0200

    master - (Fix) Fixing mv

--- docker-compile.sh
+++ docker-compile.sh
@@ -6,7 +6,7 @@ function checkVersion() {
                 echo -e "\e[1m\e[32m[+] \e[0mNew update available ..."
                 sudo curl -o docker-compile.tmp https://raw.githubusercontent.com/NastyZ98/docker-epitech/master/docker-compile.sh
                 echo -e "\e[1m\e[32m[+] \e[0mDownloaded"
-                sudo mv docker-compile docker-compile.old
+                sudo mv $0 docker-compile.old
                 sudo mv docker-compile.tmp docker-compile
                 sudo rm -f docker-compile.old
                 sudo chmod +x docker-compile
@@ -15,8 +15,6 @@ function checkVersion() {
                         export PATH=~/bin/:$PATH
                         echo -e "\e[1m\e[32m[+] \e[0m~/bin/ added to PATH"
                 fi
-                sudo mv docker-compile ~/bin/
-                echo -e "\e[1m\e[32m[+] \e[0mMoved to ~/bin/"
                 if [ -e /usr/bin/docker-compile.sh ]
                 then
                         echo -e "\e[1m\e[32m[+] \e[0mOld version found in /usr/bin/"
