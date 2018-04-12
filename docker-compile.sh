5ec2a33fd3ee592ab0cd06e8c9b351b55491589a
Nathan Agez <nathan.agez@epitech.eu>
2018-04-12 18:45:34 +0200

    master - (Fix) script location

--- docker-compile.sh
+++ docker-compile.sh
@@ -2,7 +2,7 @@
 
 function checkVersion() {
         version=$(curl -s https://raw.githubusercontent.com/NastyZ98/docker-epitech/master/version | head -n 1 )
-        if [ "$version" != "1.2" ]; then
+        if [ "$version" != "1.3" ]; then
                 echo -e "\e[1m\e[32m[+] \e[39mUpdating ..."
                 sudo curl -o docker-compile.tmp https://raw.githubusercontent.com/NastyZ98/docker-epitech/master/docker-compile.sh
                 sudo mv $0 docker-compile.old
