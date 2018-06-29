d96d57527b8aa45e345309e213a06e8b6d2d91ee
Nathan Agez <agez@MacBook-Pro-de-Nathan.local>
2018-06-29 17:31:33 +0200

    master - (Addition) macOS script

---
+++ macOS/docker-compile.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+function display_help() {
+        echo -e "\e[1mUSAGE :\e[0m\n\t docker-compile \e[1m[FLAG] [FLAG] [OPTION]\e[0m"
+        echo -e "\e[1mFLAGS :\e[0m"
+        echo -e "\t-m          Compile project"
+        echo -e "\e[1mOPTIONS :\e[0m"
+        echo -e "\t-v          Start valgrind after compilation, binary name is required"
+        echo -e "\t-i          Start project after compilation, binary name is required"
+        exit
+}
+
+function copyWorkspace() {
+        docker cp $1 epi:/home/compilation/
+}
+
+function startContainer() {
+        docker container run -d -ti --name epi epitechcontent/epitest-docker bash &> /dev/null
+        copyWorkspace $1
+}
+
+function deleteContainer() {
+        docker container rm -f epi &> /dev/null
+}
+
+function compileProject() {
+        docker container exec epi bash -c "cd /home/compilation/ && make"
+        if [ $? != 0 ]; then
+                echo -e "\n\e[31m/!\ BUILD FAIL\e[0m"
+                return 1
+        else
+                echo -e "\n\e[92m[+] BUILD COMPLETE\e[0m"
+                return 0
+        fi
+
+}
+
+function checkImageExist() {
+
+        image=$(docker images -f "reference=epitechcontent/epitest-docker" --format "{{.Repository}}")
+
+        if [ -z "$image" ]; then
+                echo -e "\e[1m\e[32m[+] \e[0mImage not found locally, it will be dowloaded from the Hub"
+                docker pull epitechcontent/epitest-docker
+                startContainer $1
+        else
+                echo -e "\e[1m\e[32m[+] \e[0mImage found locally"
+                echo -e "\e[1m\e[32m[+] \e[0mCopy file from host to container ..."
+                startContainer $1
+        fi
+}
+
+if [ $# -eq 0 ]; then
+        display_help
+        exit
+fi
+
+if [ ! -z $1 ] && [ $2 == "-m" ]; then
+        checkImageExist $1
+        compileProject
+        if [ $? -eq 0 ] && [ ! -z $3 ]; then
+                if [ $3 == "-v" ] && [ ! -z $4 ]; then
+                        echo -e "\n\e[0m\e[96m========VALGRIND========\e[0m"
+                        docker container exec -ti epi bash -c "cd /home/compilation/ ; valgrind ./$4 $5 $6"
+                elif [ $3 == "-i" ] && [ ! -z $4 ]; then
+                        echo -e "\n\e[0m\e[96m========LIVE EXEC========\e[0m"
+                        docker container exec -ti epi bash -c "cd /home/compilation/ ; ./$4 $5 $6"
+                fi
+        fi
+        deleteContainer
+else
+        display_help
+        exit
+fi
